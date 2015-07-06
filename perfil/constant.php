4e2e9165e3434949b3ac2a17bac22ff12ca3f474
Your Name <you@example.com>
2015-07-06 10:52:45 -0300

    Cambios en perfil

--- perfil/constant.php
+++ perfil/constant.php
@@ -9,13 +9,12 @@ const GET_PERFIL = 'get' ;
 const DELETE_PERFIL = 'delete' ;
 
 const EDIT_PERFIL = 'edit' ;
-<<<<<<< HEAD
+
 const GET_PERFIL_LISTAR 	= 'ver' ;
 # vistas
-=======
+
 const VIEW_EDIT_PERFIL = 'editar' ;
 
->>>>>>> ba27ece65c8ea7d7377980c6f0c8d37fe1581dd0
 const VIEW_PERFIL_LISTAR 	= 'listar' ;
 
 ?>
